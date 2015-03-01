dbfddcf60f9155f4b8831d2f8f8e4d9419806c02
Benjamin Ritter <Benjamin3992@users.noreply.github.com>
2015-03-01 19:24:53 +0100

    First stable release

---
+++ releases/OpenWeatherDuino_deploy/OpenWeatherDuino_deploy.ino
@@ -0,0 +1,68 @@
+/*
+OpenWeatherDuino by Benjamin Ritter
+Ver. 1.3
+Visit me at Github:
+https://github.com/Benjamin3992/OpenWeatherDuino
+*/
+
+#include <DHT.h>
+#include <SPI.h>
+#include <Dns.h>
+#include <Ethernet.h>
+#include <EthernetClient.h>
+
+#define server "www.openweathermap.org"  //Do not change!
+#define CredBase64 ""  //Enter here the BASE64 encoded Credentials in the form <Username>:<Password>
+#define DhtPin 3  //DHT Temperature and Humidity Sensor PIN goes here
+#define timeBetweenPush 10 //Time in minutes between two pushes goes here
+
+const String StationName = "OpenWeatherDuino1";  //Enter the Station Name how it will be displayed on Openweathermap.org
+const String lat = "10.0000000";  //Latitude goes here
+const String lng = "10.00000000";  //Longitude goes here
+//const String dew =;  //WIP!!!
+const String alt = "518";  //Enter your Altitude in meters
+
+
+byte mac[] = {0x46, 0x46, 0x46, 0x46, 0x46, 0x46};  //Enter a HEX-coded MAC-Adress here
+const IPAddress ip(192, 168, 178, 200);  //Enter the IP your Arduino will get here
+EthernetClient client;
+DHT dht;
+
+void setup() {
+  dht.setup(DhtPin);
+  Ethernet.begin(mac, ip);
+}
+
+void loop() {
+  delay(1000);
+  int t=dht.getTemperature();  //getting the Temperature from the DHT11/DHT22 Sensor
+  int h=dht.getHumidity();
+  if (client.connect(server, 80)) {  //connecting to the Openweathermap server
+    client.println("POST /data/post HTTP/1.1");
+    client.print("Host: ");
+    client.println(server);
+    client.print("Authorization: Basic ");
+    client.println(CredBase64);
+    client.println("Content-Type: application/x-www-form-urlencoded");
+    client.print("Content-Length: ");
+    client.println(int(floor(log10(abs(t)))+floor(log10(abs(h))) + 1 + lat.length()+lng.length()+alt.length()+StationName.length()+38));  //Calculating the Length of the Data for the POST method
+    client.println();
+    client.print("temp=");
+    client.print(t);
+    client.print("&humidity=");
+    client.print(h);
+    client.print("&lat=");
+    client.print(lat);
+    client.print("&long=");
+    client.print(lng);
+    client.print("&alt=");
+    client.print(alt);
+    //client.print("&dewpoint=");
+    //client.print(dew);
+    client.print("&name=");
+    client.print(StationName);
+    client.println();
+    client.stop();
+    delay(timeBetweenPush*60000);
+  }
+}
